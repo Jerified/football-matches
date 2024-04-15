30945f863a367f43328ce65ff0881c7b52f2d566
Jerrified <oyedelejeremiah.ng@gmail.com>
2024-04-15 07:08:49 +0100

    cont'd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import Image from "next/image"
 import UpcomingMatches from "@/components/UpcomingMatches"
 import News from "@/components/News"
 
-// import { useRef } from "react"
-
 export default async function Home() {
   const getDatas =  await getMatchesfootball()
   const getDatasFinished = await getMatchesfootballFinished()
@@ -26,39 +24,27 @@ export default async function Home() {
   const nd = new Date()
   const dateConvert = nd.toDateString()
 
-//   const scrollRef = useRef(null)
-
-//   const scroll = (scrollOffset) => {
-//     scrollRef.current += scrollOffset
-//   }
-
   return (
     <section className=" pt-4 lg:w-[70%]">
       <div className="flex">
         <h1 className=" md:text-xl font-bold bg-[#BEF264] pl-2 pr-10 py-1 tracking-wide">Matches</h1>
         <div className=" border-b-2 border-[#BEF264] w-full"></div>
       </div>
-       <div className="flex py-6 gap-6 overflow-x-scroll whitespace-nowrap scroll-smooth">
-        {/* <div className="w-full"> */}
+       <div className="flex py-6 gap-6 overflow-x-scroll scrollbar-hide scroll-smooth">
             {matchesDatas.map((league: any) => {
                 console.log(league)
                 return (
-                <div key={league.name} className="cursor-pointer  min-w-[20%] lg:min-w-[30%] h-full">
-                    <div className="bg-black/70 p-5 rounded-[100%]">
-                        <Image className='object-contai' src={league.emblem} width={80} height={80} alt={league.name} quality={100} />
+                <div key={league.name} className="cursor-pointer  min-w-[15%] lg:min-w-[10%] ">
+                    <div className="bg-black/70 p-3 rounded-[100%] h-full">
+                        <Image className='object-contai rounded-ful h-full w-full' src={league?.emblem} width={80} height={80} alt={league.name} quality={100} />
                     </div>
-                    {/* <div className="rounded-[100%] border-2 border-black/50 px-4 py-2"> */}
-                    {/* <Image className='object-contain rounded-full border-gray-400 bg-black/70  p-3' src={league.emblem} width={70} height={70} alt={league.name} quality={100} /> */}
-                    {/* </div> */}
                     {/* <p className="text-white text-xs invisibl">{league.name}</p> */}
                 </div>
                 )
         })}
-        {/* </div> */}
    </div>
       <LiveMatches data={matchesLive} />
       <UpcomingMatches data={matchesScheduled} />
-      {/* <News /> */}
     </section>
   )
 }
--- src/components/LiveMatches.tsx
+++ src/components/LiveMatches.tsx
@@ -4,14 +4,9 @@ import {format, parseISO, differenceInMinutes } from 'date-fns'
 
 
 const Matches = ({ data }: { data: any }) => {
-    console.log(data)
-    // const getDate = new Date(data?.utcDate).toLocaleTimeString('en-US', {
-    //     hour: '2-digit',
-    //     minute: '2-digit',
-    // });
+    // console.log(data)
     const formatDate = (dateString: string) => {
         const date: any = parseISO(dateString);
-        console.log(date)
 
         const currentTime: any = new Date()
         const elapsedMinutes = Math.floor((currentTime - date) / 60000)
@@ -21,20 +16,19 @@ const Matches = ({ data }: { data: any }) => {
         const seconds =  Math.floor((elapsedMinutes % 1) * 60)
 
         const formattedDate = `${minutes.toString().padStart(2,'0')}.${seconds.toString().padStart(2,'0')}`
-        // differenceInMinutes(new Date(), date)
         return formattedDate
     }
 
     return (
         <section className="">
             <h3 className="text-white pt-5 pb-3">LIVE MATCHES</h3>
-            <div className='flex gap-6 w-full overflow-x-auto'>
-                {/* {data.length !== 0 ? */}
-                    {
+            <div className='flex gap-6 w-full overflow-x-auto scrollbar-hide'>
+                {data.length !== 0 ?
+                    (
                         data.map((live: any) => {
                             return (
                                 <div key={live.area.id} className='bg-black/80 rounded-2xl min-w-[50%] lg:min-w-[30%] flex relative overflow-hidden'>
-                                    <div className="flex flex-1 pt-6 pb-4 px-4  w-full items-center justify-between bg-no-repeat bg-[left_2rem bg-[top_2re] bg-contain "
+                                    <div className="flex flex-1 pt-6 pb-4 px-4  w-full items-center justify-between bg-no-repeat bg-[top_2re] bg-contain "
                                     style={{backgroundImage: `url(${live.competition?.emblem})`}}
                                     >
                                         <div className='flex flex-col gap-4 items-center '>
@@ -53,31 +47,10 @@ const Matches = ({ data }: { data: any }) => {
                                         </div>
                                         <div className="rounded-full w-8 h-8 bg-[#165D31] absolute bottom-6 z-50 right-[-1rem] top-right" />
                                     </div>
-                                    
-                                    {/* <div className='w-full flex items-center'>
-                                        <p className='text-sm mr-2 whitespace-nowra'>{live?.homeTeam?.name}</p>
-                                        <div className='w-[20px] h-[20px] relative '>
-                                            <Image src={live?.homeTeam?.crest!} alt='' width={30} height={30} className='object-cover' />
-                                        </div>
-                                    </div>
-                                    <div className='px-2 m-auto flex justify-center items-center bg-slate-600 rounded-md'>
-                                        {data?.status == 'IN_PLAY' ? (
-                                            <p className='py-1 text-teal-400 text-xs'>{live?.score?.fullTime.home} : {live.score?.fullTime.away}</p>
-                                        ) : (
-                                            <p className='py-1 text-teal-400 text-xs'>{getDate}</p>
-                                        )}
-                                    </div>
-                                    <div className='w-full flex items-center justify-end gap-2'>
-                                        <div className='w-[20px] h-[20px] relative'>
-                                            <Image src={live?.awayTeam?.crest!} alt='' width={30} height={30} className='object-cover' />
-                                        </div>
-                                        <p className='text-sm text-righ'>{live.awayTeam?.name}</p>
-                                    </div> */}
                                 </div>
                             )
                         })
-                    }
-                    {/* //  : <div className='rounded-lg h-[10rem] flex justify-center items-center border-[#BEF264] border-2 border-dashed text-white text-lg font-semibold tracking-wider'>No Live Matches</div>} */}
+                    ) : <div className='rounded-lg h-[10rem] flex justify-center items-center border-[#BEF264] border-2 border-dashed text-white text-lg font-semibold w-full tracking-wider'>No Live Matches</div>}
             </div>
         </section>
     )
